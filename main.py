c5729de581bff4ee388f63a1da9baef90015890b
Khardge <Keith_Hardge@yahoo.com>
2017-10-21 00:11:09 -0500

    adding build a blog

---
+++ main.py
@@ -0,0 +1,75 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:1234@localhost:8889/Build-A-Blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(1000))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+@app.route('/blog', methods=['GET', 'POST'])
+def blog():
+
+    blog_id = request.args.get("id")
+    if request.args:
+        blog = Blog.query.get(blog_id)
+        return render_template('blogentry2.html', blog=blog)
+
+    else:
+        blogs = Blog.query.all()
+
+    return render_template('blog.html', title="Build A Blog", blogs=blogs)
+
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def add_blog():
+    if request.method == 'GET':
+        return render_template('newpost.html', title='Add Blog Entry')
+
+    if request.method == 'POST':
+        blog_title = request.form['title']
+        blog_body = request.form['body']
+        title_error = ""
+        body_error = ""
+
+    if len(blog_title) < 1:
+        title_error = "title this blog!"
+        blog_title=""
+
+    if len(blog_body) < 1:
+        body_error = "blog news?"
+        blog_body =""
+
+    if title_error:
+        return render_template('newpost.html', blog_body=blog_body, title_error=title_error)
+    
+    if body_error:
+        return render_template('newpost.html',blog_title=blog_title, body_error=body_error) 
+    
+    if not title_error and not body_error:
+        new_blog = Blog(blog_title, blog_body)
+        db.session.add(new_blog)
+        db.session.commit()
+        newb = Blog.query.all() 
+        newbid = str(new_blog.id)
+
+
+        return redirect ( "/blog?id=" + newbid)
+    
+    else:
+        return render_template('newpost.html', title="Add Blog Entry", title_error=title_error, body_error=body_error, blog_title=blog_title, blog_body=blog_body)
+
+
+
+if __name__ == '__main__':
+    app.run()
